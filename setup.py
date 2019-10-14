5d4a6e8db225c55201c9a131b9625f090b22dbfd
omirajkar <onkar.m@indictranstech.com>
2019-10-14 13:49:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+#from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
+
 import re, ast
 
 # get version from __version__ variable in bench/__init__.py
